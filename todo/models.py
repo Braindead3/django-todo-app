6ec9d68d8ec41b20a025ad69276980badc4216ad
Braindead3 <malosay21@gmail.com>
2022-05-29 02:01:53 +0300

    add custom user model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    creation_time = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
